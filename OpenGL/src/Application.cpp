c23b188bbcf61a3fee5bd2bf49887ea643dac73f
baselsaad <basel.swad@gmail.com>
2022-09-07 00:59:39 +0200

    Vertex Buffer

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -1,39 +1,20 @@
+// https://docs.gl/
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 
 
-void DrawQuad()
-{
-	glBegin(GL_QUADS);
-	glVertex2d(-0.5f, 0.0f);
-	glVertex2d(0.5f, 0.0f);
-	glVertex2d(0.5f, 0.5f);
-	glVertex2d(-0.5f, 0.5f);
-	glEnd();
-}
-
-void DrawTriangle()
-{
-	glBegin(GL_TRIANGLES);
-
-	glVertex2d(-0.5f, -0.5f);
-	glVertex2d(0.5f, -0.5f);
-	glVertex2d(0.0f, 0.5f);
-
-	glEnd();
-}
-
 int main()
 {
-	GLFWwindow* window;
-
 	/* Initialize the library */
 	if (!glfwInit())
+	{
 		return -1;
+	}
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(800, 600, "OpenGL", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
@@ -42,11 +23,39 @@ int main()
 
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
-
 	//glewInit should be called after a valid OpenGL rendering context has been created
-	int state = glewInit();
-
+	GLenum state = glewInit();
+	// GPU driver and OpenGL Information
 	std::cout << glGetString(GL_VERSION) << std::endl;
+
+
+	float positions[6]
+	{
+		-0.5f,-0.5f,
+		 0.5f,-0.5f,
+		 0.0f, 0.5f
+	};
+
+	unsigned int buffer;
+	/*
+	*	n		-	Specifies the number of buffer object names to be generated.
+	*	buffer	-	Specifies the name of a buffer object.
+	*/
+	glGenBuffers(1, &buffer);
+
+	/*
+	*	target - to which the buffer object is bound
+	*	buffer - Specifies the name of a buffer object.
+	*/
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+
+	/*
+	*	target - to which the buffer object is bound
+	*	size   - Specifies the size in bytes of the buffer object's new data store.
+	*	data   - Specifies a pointer to data that will be copied into the data store for initialization, or NULL if no data is to be copied.
+	*   usage  - Specifies the expected usage pattern of the data store
+	*/
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
@@ -54,15 +63,17 @@ int main()
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		DrawTriangle();
-
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
+
+		// because we generate and bind the buffer outside the loop, OpenGL works like state-machine, so it knows which buffer should be drawn
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		/* Poll for and process events */
 		glfwPollEvents();
 	}
 
+	// free allocated
 	glfwTerminate();
 	return 0;
 }
---
+++ OpenGL/src/LegacyOpenGL.cpp
@@ -0,0 +1,71 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+
+void DrawQuad()
+{
+	glBegin(GL_QUADS);
+	glVertex2d(-0.5f, 0.0f);
+	glVertex2d(0.5f, 0.0f);
+	glVertex2d(0.5f, 0.5f);
+	glVertex2d(-0.5f, 0.5f);
+	glEnd();
+}
+
+void DrawTriangle()
+{
+	glBegin(GL_TRIANGLES);
+	glVertex2d(-0.5f, -0.5f);
+	glVertex2d(0.5f, -0.5f);
+	glVertex2d(0.0f, 0.5f);
+	glEnd();
+}
+
+#if 0
+
+int main()
+{
+	/* Initialize the library */
+	if (!glfwInit())
+	{
+		return -1;
+	}
+
+	/* Create a windowed mode window and its OpenGL context */
+	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return EXIT_FAILURE;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	//glewInit should be called after a valid OpenGL rendering context has been created
+	GLenum state = glewInit();
+
+	std::cout << glGetString(GL_VERSION) << std::endl;
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		DrawTriangle();
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	// free allocated
+	glfwTerminate();
+	return 0;
+}
+
+#endif
